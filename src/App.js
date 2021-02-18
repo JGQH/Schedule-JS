b058d3c8196819d962095172e545b1b496eb1f1d
Juan Quispe <jquispeh63@gmail.com>
2021-02-17 21:41:43 -0500

    Creates basic template
    -Creates Selector component (Stub)
    -Adds options to App named selectors
    -Creates styles files using SASS for main and selectors
    -Adds main and selector CSS to indedx HTML
    -Modifies gitignore to ignore .css, .css.map files and vscode settings

--- src/App.js
+++ src/App.js
@@ -1,6 +1,38 @@
+import { useState } from 'react';
+import Selector from './Selector';
+
 const App = () => {
+  const [tab, setTab] = useState(0)
+  const selectors = [
+    {
+      "text":"Tutorial"
+    },
+    {
+      "text":"Crear bloques"
+    },
+    {
+      "text":"Visualizar cursos"
+    },
+    {
+      "text":"Horarios generados"
+    }
+  ];
   return (
-    <p>Hello World!</p>
+    <>
+    <div className="selector-container">
+      {(window.innerWidth < 768) ? //Arrows to change tabs
+      <>
+        <div className="selector-arrow-left" onClick={() => (tab > 0) && setTab(tab - 1)}></div>
+        <Selector text={selectors[tab].text}/>
+        <div className="selector-arrow-right" onClick={() => (tab < (selectors.length - 1)) && setTab(tab + 1)}></div>
+      </>
+      : //List of tabs
+      selectors.map((selector, index) => {
+        return <Selector key={index} text={selector.text}/>
+      })
+      }
+    </div>
+    </>
   );
 }
 
---
+++ src/Selector.js
@@ -0,0 +1,9 @@
+const Selector = ({ text, onClick }) => {
+    return (
+        <div className="selector-option">
+            <p>{text}</p>
+        </div>
+    );
+}
+
+export default Selector;
