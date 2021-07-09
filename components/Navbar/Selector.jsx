20d456390903b21335073e287300ee09d2094659
Juan Quispe <jquispeh63@gmail.com>
2021-07-09 18:28:58 -0500

    Modifies color palette
    - Rewrites the entire color palette and how to import them
    - Modifies styling of navbar options

--- components/Navbar/Selector.jsx
+++ components/Navbar/Selector.jsx
@@ -2,7 +2,7 @@ import styles from './Navbar.module.scss'
 
 const Selector = ({ text, onClick, selected }) => {
     return (
-        <div className={`${styles.navOption} ${selected && styles.navSelected}`} onClick={() => onClick && onClick()}>
+        <div className={selected ? styles.navSelected : styles.navOption} onClick={() => onClick && onClick()}>
             <h4>{text.toUpperCase()}</h4>
         </div>
     );
