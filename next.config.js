d95e0c3c8a8f124041dcd0e5f5e433210b5c54ac
Juan Quispe <jquispeh63@gmail.com>
2021-07-09 23:12:07 -0500

    Modifies config file
    - Modifies basePath based on environment for the github pages

--- next.config.js
+++ next.config.js
@@ -1,10 +1,11 @@
 const { resolve } = require('path')
-
+const isProduction = process.env.NODE_ENV === 'production'
 
 module.exports = {
     sassOptions: {
         includePaths: [
             resolve(__dirname, 'styles')
         ]
-    }
+    },
+    basePath: isProduction ? '/Schedule-JS' : ''
 }
